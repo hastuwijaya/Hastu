874e7cbde5c9df2e8b110f140654282e3ce020e9
Hastuwijaya <hastuayu@gmail.com>
2019-02-19 13:44:11 +0700

    menambahkan file project pertama

---
+++ new 2.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Judul</title>
+<style type="text/css">
+	
+	* {
+		margin: 0;
+		padding:0;
+	}
+	#menu {
+		width: 100%;
+		height: 40px;
+		background-color: red;
+	}
+	.item {
+		width: 97%;
+		margin: 1%;
+		height: 200px;
+		padding: 5px;
+		background-color: #ecf0f1;	
+	}
+	#color_menu {
+		color: white;
+		padding: 3px;
+	}
+	.thumbnail {
+		width: 40%;
+		height: 195px;
+		float: left;
+		background-color: #bdc3c7;
+	}
+	.deskripsi {
+		width: 55%;
+		height: 195px;
+		float: left;
+		margin-left: 20px;
+		background-color: white;
+	}
+	#icon {
+		float: left;
+		margin-right:5px;
+		margin-left: 5px;
+		margin-top: 10px;
+	}
+	.tagline{
+		width: 95%;
+		background-color: red;
+		height: 20px;
+		margin: 10px;
+	}
+	.profile{
+		width: 20%;
+		height: 20px;
+		background-color: blue;
+		float: left;
+	}
+	.judul{
+		width: 75%;
+		height: 20px;
+		background-color: green;
+		float: left;
+	}
+
+</style>
+</head>
+<body>
+
+	<div id="menu" style="margin-bottom: 20px;">
+	<img id="icon" src="assets1.jpg" height="20" width="20">
+		<h2 id="color_menu">YouTube</h2>
+	</div>
+
+	<div class="item">
+		<div class="thumbnail"></div>
+		
+		<div class="deskripsi"></div>
+		<div class="tagline">
+		<div class="profile">
+		</div>
+		<div class="judul">
+		</div>
+		</div>
+		</div>
+	</div>
+	
+	<div class="item">
+		<div class="thumbnail"></div>
+		<div class="deskripsi"></div>
+	</div>
+	
+	<div class="item">
+		<div class="thumbnail"></div>
+		<div class="deskripsi"></div>
+	</div>
+	
+	<div class="item">
+		<div class="thumbnail"></div>
+		<div class="deskripsi"></div>
+	</div>
+
+</body>
+</html>
